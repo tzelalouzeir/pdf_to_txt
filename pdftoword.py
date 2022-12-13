666da70270e72a4eda19a021820238e76ec73f84
Tzelal O <85295561+tzelalouzeir@users.noreply.github.com>
2022-12-13 09:39:21 +0300

    Add files via upload

---
+++ pdftoword.py
@@ -0,0 +1,28 @@
+# Import the required libraries
+from io import StringIO
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+
+# Set up the PDFMiner objects
+rsrcmgr = PDFResourceManager()
+sio = StringIO()
+laparams = LAParams()
+device = TextConverter(rsrcmgr, sio, codec='utf-8', laparams=laparams)
+interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+# Open the PDF file and extract its text
+with open('document.pdf', 'rb') as fp:
+    for page in PDFPage.get_pages(fp):
+        interpreter.process_page(page)
+
+# Close the PDFMiner objects and get the text from the StringIO object
+device.close()
+pdf_text = sio.getvalue()
+sio.close()
+
+# Save the extracted text to a Word document
+with open('document.docx', 'w') as fp:
+    fp.write(pdf_text)
